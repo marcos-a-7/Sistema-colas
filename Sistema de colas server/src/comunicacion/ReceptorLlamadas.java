55148df0bb56433e9bb5439171ddfaf3818e7123
Marcos <marcos.aguilera.7.13@gmail.com>
2021-05-04 20:00:52 -0300

    agrego receptor de llamadas en servidor

---
+++ Sistema de colas server/src/comunicacion/ReceptorLlamadas.java
@@ -0,0 +1,74 @@
+package comunicacion;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import modelo.AsignadorTurnos;
+
+public class ReceptorLlamadas
+{
+	private static ReceptorLlamadas instance;
+	private AsignadorTurnos asignadorTurnos;
+	private int port;
+
+	private ReceptorLlamadas()
+	{
+		super();
+		this.port = 10000;// obtener de un txt cfg
+		this.asignadorTurnos = AsignadorTurnos.getInstance();
+		recibirLlamado();
+	}
+
+	public static ReceptorLlamadas getInstance()
+	{
+		if (instance == null)
+		{
+			instance = new ReceptorLlamadas();
+		}
+		return instance;
+	}
+
+	private void recibirLlamado()
+	{
+		ServerSocket server = null;
+		Socket socket = null;
+		DataInputStream in;
+		DataOutputStream out;
+
+		try
+		{
+			server = new ServerSocket(port);
+			while (true)
+			{
+				socket = server.accept();
+
+				in = new DataInputStream(socket.getInputStream());
+				out = new DataOutputStream(socket.getOutputStream());
+
+				int box = in.read();
+				String dniSig = this.asignadorTurnos.llamarSiguiente(box);// envio el box para llamar al siguiente
+
+				out.writeUTF(dniSig);// devuelvo el dni del siguiente al sistema de empleado
+				socket.close();
+			}
+		} catch (IOException e)
+		{
+			e.printStackTrace();
+		} finally
+		{
+			if (server != null)
+			{
+				try
+				{
+					server.close();
+				} catch (IOException e)
+				{
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+}
