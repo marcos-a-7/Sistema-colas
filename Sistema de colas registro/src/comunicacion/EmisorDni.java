3bf4e8227c4dd5c1872de3eef1a0891878d306f8
Marcos <marcos.aguilera.7.13@gmail.com>
2021-05-05 18:02:50 -0300

    creo emisor dni

--- Sistema de colas registro/src/comunicacion/EmisorDni.java
+++ Sistema de colas registro/src/comunicacion/EmisorDni.java
@@ -27,6 +27,38 @@ public class EmisorDni
 		return instance;
 	}
 
+	/**
+	 * @return the port
+	 */
+	public int getPort()
+	{
+		return port;
+	}
+
+	/**
+	 * @param port the port to set
+	 */
+	public void setPort(int port)
+	{
+		this.port = port;
+	}
+
+	/**
+	 * @return the host
+	 */
+	public String getHost()
+	{
+		return host;
+	}
+
+	/**
+	 * @param host the host to set
+	 */
+	public void setHost(String host)
+	{
+		this.host = host;
+	}
+
 	public boolean enviarCliente(String dni)
 	{
 		boolean salida = false;
