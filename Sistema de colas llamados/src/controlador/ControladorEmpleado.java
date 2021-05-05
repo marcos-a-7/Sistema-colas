db8fff32af7eb91802c6832663093e139d32fa68
Marcos <marcos.aguilera.7.13@gmail.com>
2021-05-05 18:03:29 -0300

    agrego controlador empleado
    Falta leer cfg y demas

---
+++ Sistema de colas llamados/src/controlador/ControladorEmpleado.java
@@ -0,0 +1,49 @@
+package controlador;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import comunicacion.EmisorLLamados;
+import comunicacion.Mensaje;
+import vista.Interfaz_Empleado;
+
+public class ControladorEmpleado implements ActionListener
+{
+	private Interfaz_Empleado ventana;
+	private int box;
+	private EmisorLLamados llamador;
+
+	public ControladorEmpleado(int box)
+	{
+		super();
+		this.box = box;
+		this.ventana = new Interfaz_Empleado(box);
+		llamador = EmisorLLamados.getInstance();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e)
+	{
+		String comando = e.getActionCommand();
+
+		if (comando.equals("LLAMAR"))
+		{
+			this.llamarNuevo();
+		}
+	}
+
+	private void llamarNuevo()
+	{
+		Mensaje mensaje = llamador.enviarLlamado(this.box);
+
+		if (mensaje.getDni().isEmpty())
+		{
+			this.ventana.imprimeMensaje("No hay personas en la cola de esper, llame nuevamente mas tarde");
+		} else
+		{
+			this.ventana.setDni(mensaje.getDni());
+			this.ventana.setCantidadClientes(mensaje.getCantCola());
+		}
+	}
+
+}
